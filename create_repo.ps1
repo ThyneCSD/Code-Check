4acbcb447abcca5d1c590d06429fc779114a741d
Thyne <Thyneschool@gmail.com>
2026-07-04 23:05:42 +0200

    Replace token in script with a placeholder

--- create_repo.ps1
+++ create_repo.ps1
@@ -1,4 +1,4 @@
-$token = "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"
+$token = "YOUR_GITHUB_TOKEN"
 $headers = @{ Authorization = "token $token"; Accept = "application/vnd.github+json" }
 $body = @{ name = "code-checker"; private = $false; description = "Simple code verification site" } | ConvertTo-Json
 Invoke-RestMethod -Method Post -Uri "https://api.github.com/user/repos" -Headers $headers -Body $body -ContentType "application/json"
